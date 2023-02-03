0eedcfe582c3a162d58b380a4df3803adaa08a4b
maggie <mjtice0@gmail.com>
2023-02-03 08:34:18 -0700

    Back-end code for capstone project

---
+++ routes/places.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+const { default: axios } = require("axios");
+const express = require("express");
+const request = require("express");
+
+router.get("", async (req, res, next) => {
+  try {
+    const apiKey = "AIzaSyBDPbahXMrfa37wb_ISDP3FFJ7z6n5H7BI";
+    // const query = 'restaurants';
+    const query =
+      "Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry";
+    const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${query}&key=${apiKey}`;
+    console.log(url);
+    axios.get(url).then((response) => {
+      res.status(200).send(response.data);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   router.listen(8800, () => {
+//     console.log('Server running on port 3000');
+//   });
+
+// https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=AIzaSyBDPbahXMrfa37wb_ISDP3FFJ7z6n5H7BI
+
+//  const place = await axios.get("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry&key=")
+// console.log(data);
+// res.status(200).json(data)
+//   }catch(err) {
+//     res.status(500).json(err)
+//   }
+// })
+
+module.exports = router;
