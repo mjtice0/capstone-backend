b70ef4f33c971ec447a4ec22fe33390cbdc47ecf
maggie <mjtice0@gmail.com>
2023-02-06 16:16:03 -0700

    Added places file to connect to external API. Confirmed that connection is working

--- routes/places.js
+++ routes/places.js
@@ -1,5 +1,25 @@
+const router = require("express").Router();
+const { default: axios } = require("axios");
+const express = require("express");
+const request = require("express");
 
+router.get("", async (req, res, next) => {
+  try {
+    const apiKey = prcocess.env(GOOGLE_URL)
+    // const query = 'restaurants';
+    const query =
+      "Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=formatted_address%2Cname%2Crating%2Copening_hours%2Cgeometry";
+    const url = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${query}&key=${apiKey}`;
+    console.log(url);
+    axios.get(url).then((response) => {
+      res.status(200).send(response.data);
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+module.exports = router;
 
 
 
@@ -32,4 +52,3 @@
 //   }
 // })
 
-module.exports = router;
